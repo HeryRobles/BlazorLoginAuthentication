2d73b1a9b4d899ddce45155cc8cf4fd873d4b56a
Heriberto Robles Coronado <rrobleshc@hotmail.com>
2023-08-02 20:17:16 -0700

    Add project files.

---
+++ BlazorLogin/Models/Entities/Token.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorLogin.Models.Entities
+{
+    public class Token
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string TokenJWT { get; set; }
+
+        [Required]
+        public DateTime Vigencia { get; set; }
+    }
+}
---
+++ BlazorLogin/Program.cs
@@ -0,0 +1,18 @@
+using BlazorLogin;
+using BlazorLogin.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProviderFalse>();
+
+await builder.Build().RunAsync();
