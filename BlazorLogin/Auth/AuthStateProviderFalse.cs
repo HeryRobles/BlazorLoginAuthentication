8937466b2f21dbc8b73f3d633287460ee2256c32
Heriberto Robles Coronado <rrobleshc@hotmail.com>
2023-08-02 21:30:35 -0700

    Authoritation Roles

--- BlazorLogin/Auth/AuthStateProviderFalse.cs
+++ BlazorLogin/Auth/AuthStateProviderFalse.cs
@@ -8,7 +8,15 @@ namespace BlazorLogin.Auth
         public async override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             var anonimo = new ClaimsIdentity();
-            return await Task.FromResult(new AuthenticationState (new ClaimsPrincipal(anonimo)));
+            var usuarioHery = new ClaimsIdentity(
+                new List<Claim>
+                {
+                    new Claim("llave1", "valor1"),
+                    new Claim("mugiwara", "999"),
+                    new Claim(ClaimTypes.Name, "Heriberto"),
+                },
+                authenticationType: "prueba");
+            return await Task.FromResult(new AuthenticationState (new ClaimsPrincipal(usuarioHery)));
         }
     }
 }
